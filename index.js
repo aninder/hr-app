f217287f7e88b6d031b0e9db6654fff6faf4261e
aninder <aninder@gmail.com>
2015-03-06 19:36:01 +0530

    moved response generation to a separate module,now also serving static files,refactoring

--- index.js
+++ index.js
@@ -1,45 +1,33 @@
 var http = require('http');
-var emp = require('./lib/employee');
+var employeeService = require('./lib/employee');
+var responseGenerator = require('./lib/responseGenerator');
 var host='127.0.0.1';
 var port=9000;
 
 server = http.createServer(function (request, response) {
     console.log(request.method + ' ' + request.url);
     if (request.method !== 'GET') {
-        response.writeHead(501, {
-            'Content-Type': 'text/plain'
-        });
-        return response.end(request.method + ' not implemented by this ➥server.');
+        return responseGenerator.send501(request.method + ' not implemented by this ➥server.', response);
     }
     //all employees
     if (/^\/emp$/i.exec(request.url)) {
-        emp.getEmployees(function(error,employees){
+        employeeService.getEmployees(function(error,employees){
             if(error){
-                response.writeHead(502);
-                return response.end(error);
-            } else {
-            response.writeHead(200);
-            employees.forEach(function(employee){
-                response.write(employee.id+' '+employee.name+' '+employee.address+'\n');
-            })}
-        });
-        return response.end();
-    } else
-    //some emp with id
-    var match;
-    if (match = /^\/emp\/(\d+)$/i.exec(request.url)) {
-        emp.getEmployee(match[1],function(error,employee) {
-            if(error) {
-                response.writeHead(200);
-                return response.end(error);
+                return responseGenerator.send404(error, response);
             }
-            response.writeHead(200);
-            response.write(employee.id+' '+employee.name+' '+employee.address);
-            return response.end('some emp');
+                return responseGenerator.sendJson(employees, response);
         });
+    } else if (match = /^\/emp\/(\d+)$/i.exec(request.url)) {
+        //some employeeService with id
+        employeeService.getEmployee(match[1],function(error,employee) {
+            if(error) {
+                return responseGenerator.send404(error, response);
+            }
+            return responseGenerator.sendJson(employee, response);
+        });
+    } else {
+        responseGenerator.staticFile('/public')(request.url, response);
     }
-        response.writeHead(200);
-        return response.end('wtf ??');
 });
 
 server.on('listening', function(){
